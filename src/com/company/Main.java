0bba4aeaafffe05476ae475da27b304b866802f7
Joyce Yang <jcyang@hmc.edu>
2017-10-16 10:06:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int maxNum=0;
+	Scanner scan = new Scanner(System.in);
+        //User can enter at most the maximum number of books
+        System.out.println("Maximum number of books: ");
+
+        maxNum=scan.nextInt();
+        //Force scanner to see line break
+        String lineBreak=scan.nextLine();
+        String[] titleArray = new String[maxNum];
+        String[] authorArray = new String[maxNum];
+        String[] descriptionArray = new String[maxNum];
+	int i=0;
+String repeatSwitch ="";
+
+    while (!repeatSwitch.equalsIgnoreCase("Quit")) {
+        //check to make sure that i is less than length of array.
+        if (i>=titleArray.length){
+            break;
+        }
+        System.out.println("Enter the book title: ");
+        titleArray[i] = scan.nextLine();
+        System.out.println("Enter the author: ");
+        authorArray[i] = scan.nextLine();
+        System.out.println("Enter the description: ");
+        descriptionArray[i] = scan.nextLine();
+        System.out.println("Would you like to continue? Enter any key to continue or Quit to exit");
+        repeatSwitch= scan.nextLine();
+        i++;
+    }
+
+    int total=i;
+    for(int j =0; j<total; j++){
+        String bookString="";
+        bookString+= "Title: "+ titleArray[j] ;
+        bookString+= " \n Author: " +authorArray[j];
+        bookString+= " \n Description: "+descriptionArray[j];
+
+        System.out.println(bookString);
+    }
+
+
+
+
+
+
+
+    }
+}
